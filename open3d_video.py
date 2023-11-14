dc36bb59fd0f6cb818ecd6dfafeaa2db31553fc0
andreabassi78 <andreabassi@polimi.it>
2023-11-14 23:18:43 +0100

    both moon and earth move

--- open3d_video.py
+++ open3d_video.py
@@ -7,45 +7,58 @@ from my_open3d_utils import compute_edges
 
 radius_earth = 1.0
 radius_moon = 0.3
-pos = np.array((6.0, 1.0, 0.0)) # initial position of the moon
-vel = np.array((0.0,-0.3,0.9)) # initial velocity
-G = 6 # gravitational constant
-M = 1.0 # earth mass
-m = 0.1 # moon mass
-N = 400 # number of frames in the movie
+G = 1 # gravitational constant
+M = 5.0 # earth mass
+m = 0.5 # moon mass
+N = 600 # number of frames in the movie
+pos_moon = np.array((3.0, -1.0, 0.0)) # initial position of the moon
+vel_moon = np.array((0.0, -0.4, 1.3)) # initial velocity of the moon
+pos_earth = np.array((0.0, 0.0, 0.0)) # initial position of the earth
+vel_earth = -vel_moon*m/M # initial velocity of the earth with conservation of momentum
+
 
 save_video = False
 full_path = os.path.realpath(__file__) 
 folder, _ = os.path.split(full_path) # save folder for the video 
 
 earth = o3d.geometry.TriangleMesh.create_icosahedron(radius=radius_earth) 
-earth.compute_vertex_normals()
 moon = o3d.geometry.TriangleMesh.create_icosahedron(radius=radius_moon) 
-moon.translate(pos)
+moon.translate(pos_moon)
 
 vis = o3d.visualization.Visualizer()
 vis.create_window()
 vis.add_geometry(earth)
 vis.add_geometry(moon)
 
-dt = 0.1
+dt = 0.05
 
 for i in range(N):
 
-    acc = - G*M* pos/norm(pos)**3
-    vel += acc * dt
-    dr = vel * dt
-    pos += dr
-    print('distance from the earth:', norm(pos))
+    distance = pos_moon-pos_earth
+    force = - G*M*m * distance/norm(distance)**3 # force on the moon
+    acc_moon = force/m
+    acc_earth = -force/M
+    
+    vel_moon += acc_moon * dt
+    vel_earth += acc_earth * dt
+    
+    dr_moon = vel_moon * dt
+    dr_earth = vel_earth * dt
+    
+    pos_moon += dr_moon
+    pos_earth += dr_earth
+    print('distance from Earth-Moon:', norm(distance))
+    print('distance of the Moon from the origin:', norm(pos_moon))
 
-    rotation_angle = -0.5*2*np.pi/N
-    rotation = earth.get_rotation_matrix_from_axis_angle((0, rotation_angle,0))
-    earth.rotate(rotation, center=(0, 0, 0))
+    rotation_angle_earth = 2*np.pi/N
+    rotation = earth.get_rotation_matrix_from_axis_angle((0, rotation_angle_earth,0))
+    earth.rotate(rotation, center=pos_earth)
+    earth.translate(dr_earth)
     
     rotation_angle_moon = 2*2*np.pi/N
     revolution = moon.get_rotation_matrix_from_axis_angle((0,rotation_angle_moon,0))
-    moon.rotate(revolution, center=pos)
-    moon.translate(dr)
+    moon.rotate(revolution, center=pos_moon)
+    moon.translate(dr_moon)
     
     vis.update_geometry(earth)
     vis.update_geometry(moon)
