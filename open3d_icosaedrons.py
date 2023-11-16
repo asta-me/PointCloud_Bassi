maximum distance calculation corrected

open3d_icosaedrons.py
@@ -1,8 +1,9 @@
 import open3d as o3d
 import numpy as np
 from my_open3d_utils import compute_edges
+from numpy.linalg import norm
 
-earth = o3d.geometry.TriangleMesh.create_icosahedron(radius=0.15) 
+earth = o3d.geometry.TriangleMesh.create_icosahedron(radius=0.1) 
 earth.compute_vertex_normals()
 R = earth.get_rotation_matrix_from_xyz((0.35* np.pi, 0, 0.26*np.pi))
 earth.rotate(R, center=(0, 0, 0))
@@ -10,9 +11,9 @@ triangles = np.array(earth.triangles)
 vertices = np.array(earth.vertices)
 originsE, endsE = compute_edges(earth)
 
-moon = o3d.geometry.TriangleMesh.create_icosahedron(radius=0.07) 
+moon = o3d.geometry.TriangleMesh.create_icosahedron(radius=0.05) 
 moon.compute_vertex_normals()
-moon.translate((0.8, 0, -0.8))
+moon.translate((0.5, 0, -0.7))
 R = moon.get_rotation_matrix_from_xyz((0.27*np.pi, 0, 0.25*np.pi))
 moon.rotate(R, center=moon.get_center())
 triangles = np.array(moon.triangles)
@@ -24,7 +25,6 @@ origins = np.concatenate((np.array(originsM),np.array(originsE)),axis=0)
 ends = np.concatenate((np.array(endsM),np.array(endsE)),axis=0)
 
 print('Number of edges:', origins.shape)
-print('Maximum distance from the center of the field of view:', np.max(origins))
-
+print('Maximum displacement from the center of the field of view:', np.max(np.abs(origins),axis=0))
 o3d.visualization.draw_geometries([earth,moon])
 

open3d_sphere.py
@@ -10,5 +10,5 @@ earth.rotate(R, center=(0, 0, 0))
 
 origins, ends = compute_edges(earth)
 print('Number of edges:', origins.shape)
-print('Maximum distance from the center of the field of view:', np.max(origins))
+print('Maximum displacement from the center of the field of view:', np.max(np.abs(origins),axis=0))
 o3d.visualization.draw_geometries([earth])

open3d_tetrahedron.py
@@ -1,11 +1,12 @@
 import open3d as o3d
 import numpy as np
 from my_open3d_utils import compute_edges
+from numpy.linalg import norm
                          
 mesh = o3d.geometry.TriangleMesh.create_tetrahedron()
 mesh.compute_vertex_normals()
 mesh.compute_adjacency_list()
 origins, ends = compute_edges(mesh)
 print('Number of edges in the shape:', origins.shape[0])
-print('Maximum distance from the center of the field of view:', np.max(origins))
+print('Maximum displacement from the center of the field of view:', np.max(np.abs(origins),axis=0))
 o3d.visualization.draw_geometries([mesh])

open3d_video_Nbody_problem.py
@@ -131,6 +131,6 @@ for i in range(time_points):
             counts+=1
     
     # print(f'Calculating {origins.shape[0]} edges for rendering {counts} bodies')
-    print('Maximum distance from the center of the field of view:', np.max(origins)) # maximun value of x,y or z
+    print('Maximum displacement from the center of the field of view:', np.max(np.abs(origins),axis=0))
              
 vis.destroy_window()

open3d_video_escaping planet.py
@@ -11,11 +11,11 @@ G = 1 # gravitational constant
 M = 5.0 # earth mass
 m = 0.5 # moon mass
 N = 600 # number of frames in the movie
-pos_moon = np.array((0.8, -0.9, 0.0)) # initial position of the moon
-vel_moon = np.array((0.0, -0.2, 1.6)) # initial velocity of the moon
-pos_earth = np.array((0.0, 0.0, 0.0)) # initial position of the earth
+pos_moon = np.array((0.8, -0.9, -1.0)) # initial position of the moon
+vel_moon = np.array((0.0, -0.2, -3.0)) # initial velocity of the moon
+pos_earth = np.array((0.0, 0.0, -1.0)) # initial position of the earth
 vel_earth = -vel_moon*m/M # initial velocity of the earth with conservation of momentum
-
+vel_earth+=np.array((0.0, 0.0, -1))
 
 save_video = False
 full_path = os.path.realpath(__file__) 
@@ -74,6 +74,8 @@ for i in range(N):
 
     origins = np.concatenate((originsM,originsE))
     ends = np.concatenate((endsM,endsE))
+
+    #print(f'Calculating {origins.shape[0]} edges')
     print('Maximum displacement from the center of the field of view:', np.max(np.abs(origins),axis=0))
 
 vis.destroy_window()