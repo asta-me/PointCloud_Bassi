8309ce009a2d092ded78e9db8b7f414c98cb24a7
Marco Astarita <astamarco98@gmail.com>
2023-12-05 11:14:01 +0100

    Corrected escaping planets
    Escaping planet ready for calculation

--- test_my_open3d_API.py
+++ test_my_open3d_API.py
@@ -14,7 +14,7 @@ depth=0.5;
 #Frameindex
 i=1;
 
-for mesh_list in escaping_planet(time_points=3,
+for mesh_list in escaping_planet(time_points=600,
                                 show_animation=True,
                                 remove_hidden_lines=False,
                                 show_mesh=False,
@@ -30,11 +30,15 @@ for mesh_list in escaping_planet(time_points=3,
     x1,y1,z1,x2,y2,z2=oe_to_input(origins,ends,depth,lam,d,res,margin=0.005,image_extent=2);
     
     print(f"Computing {i} of 3")
-    print('Maximum displacement from the center of the field of view:', np.max(np.abs(origins),axis=0))
-    print(f'This time-point has {points.shape[0]} points and {lines.shape[0]} lines')
+    # print('Maximum displacement from the center of the field of view:', np.max(np.abs(origins),axis=0))
+    print('x max ', np.max((x1,x2)), 'x min ', np.min ((x1,x2)))
+    print('y max ', np.max((y1,y2)), 'y min ', np.min ((y1,y2)))
+    print('z max ', np.max((z1,z2)), 'z min ', np.min ((z1,z2))) 
     
-    rs_phase, performance = rs(x1, y1, z1, x2, y2, z2, d, lam, res)
-    rs_phase_bmp=phase_to_bmp(phase=rs_phase);
-    save_frame(rs_phase_bmp,i,title="minitest",subfolder="minitest_folder")    
+    # print(f'This time-point has {points.shape[0]} points and {lines.shape[0]} lines')
+    
+    # rs_phase, performance = rs(x1, y1, z1, x2, y2, z2, d, lam, res)
+    # rs_phase_bmp=phase_to_bmp(phase=rs_phase);
+    # save_frame(rs_phase_bmp,i,title="minitest",subfolder="minitest_folder")    
     i+=1;
 
